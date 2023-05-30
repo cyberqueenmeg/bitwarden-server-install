5751f1e4f2ba4bf2223a84b67be41ef7c61584d5
Megan Howell <62813469+cyberqueenmeg@users.noreply.github.com>
2023-05-29 20:24:26 -0700

    Add files via upload

---
+++ bitwardeninstall.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Configure DNS and your web domain before starting
+
+# Install Docker Engine & Docker Compose (ubuntu specific)
+sudo apt-get update -y
+sudo apt-get install -y ca-certificates curl gnupg
+sudo install -y -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
+
+# Create bitwarden user and directory
+sudo adduser bitwarden
+sudo passwd bitwarden 
+sudo groupadd docker
+sudo usermod -aG docker bitwarden
+sudo mkdir /opt/bitwarden
+sudo chmod -R 700 /opt/bitwarden
+sudo chown -R bitwarden:bitwarden /opt/bitwarden
+su bitwarden
+cd /opt/bitwarden
+
+# Download and install bitwarden server
+curl -Lso bitwarden.sh "https://func.bitwarden.com/api/dl/?app=self-host&platform=linux" && chmod 700 bitwarden.sh
+./bitwarden.sh install
